95a76c500ed3d167527c9888ca01327c2a38969e
Ghosty <75956731+Kurokeida@users.noreply.github.com>
2023-09-10 02:52:44 +0200

    Update MainActivity.kt

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,52 +1,59 @@
-package com.example.calculator
+package com.example.multiplicationtable
 
-import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
-import org.w3c.dom.Text
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+
+
+//Reference's
+//https://stackoverflow.com/questions/7400475/how-to-remove-listview-all-items
+// https://www.geeksforgeeks.org/how-to-dynamically-add-elements-to-a-listview-in-android/
+// https://www.youtube.com/watch?v=7yZngVxaZDM
+//https://guides.codepath.com/android/Using-an-ArrayAdapter-with-ListView
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId", "WrongViewCast")
+
+
+    lateinit var lngList: ArrayList<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var MultiplicationList = findViewById<ListView>(R.id.MultiplicationList)
+        var DObtn = findViewById<Button>(R.id.DObtn)
+        var NumberInput = findViewById<EditText>(R.id.NumberInput)
+        var sum = 0;
+        lngList = ArrayList()
 
-        var AnswerDisplay = findViewById<TextView>(R.id.AnswerView)
-        var FirstValue = findViewById<EditText>(R.id.FirstValue)
-        var SecondValue = findViewById<EditText>(R.id.SecondValue)
-
-        var PlusButton = findViewById<Button>(R.id.PlusButton)
-        var MinusButton = findViewById<Button>(R.id.MinusButton)
-        var TimesButton = findViewById<Button>(R.id.TimesButton)
-        var DivideButton = findViewById<Button>(R.id.DivideButton)
-        var SQRTButton = findViewById<Button>(R.id.SQRTButton)
+        val adapter: ArrayAdapter<String?> = ArrayAdapter<String?>(
+            this@MainActivity,
+            android.R.layout.simple_list_item_1,
+            lngList as List<String?>
+        )
 
 
-        PlusButton.setOnClickListener {
-           var DifferenceStorage = FirstValue.text.toString().toInt() + SecondValue.text.toString().toInt()
-            AnswerDisplay.text = DifferenceStorage.toString()
-        }
-        MinusButton.setOnClickListener {
-            var DifferenceStorage = FirstValue.text.toString().toInt() - SecondValue.text.toString().toInt()
-            AnswerDisplay.text = DifferenceStorage.toString()
-        }
-        TimesButton.setOnClickListener {
-            var DifferenceStorage = FirstValue.text.toString().toInt() * SecondValue.text.toString().toInt()
-            AnswerDisplay.text = DifferenceStorage.toString()
-        }
-        DivideButton.setOnClickListener {
-            var DifferenceStorage = FirstValue.text.toString().toInt() / SecondValue.text.toString().toInt()
-            AnswerDisplay.text = DifferenceStorage.toString()
-        }
-        SQRTButton.setOnClickListener {
-            var DifferenceStorage = Math.sqrt(FirstValue.text.toString().toInt().toDouble())
-            AnswerDisplay.text = DifferenceStorage.toString()
-        }
 
+        // on below line we are adding click listener for our button.
+        DObtn.setOnClickListener {
+            val item = NumberInput.text.toString()
+            if (item.isNotEmpty()) {
+                var MultiplicationNumber = 1
+                adapter.clear()
+                MultiplicationList.adapter = adapter
+                for(i in 1..12) {
+                    sum = NumberInput.text.toString().toInt() * MultiplicationNumber
+                    lngList.add(NumberInput.text.toString()+" X "+MultiplicationNumber.toString() + " = " + sum)
+                    MultiplicationNumber++
+                }
+
+
+                adapter.notifyDataSetChanged()
+            }
+        }
 
     }
 }
