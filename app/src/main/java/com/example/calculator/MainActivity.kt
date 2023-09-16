7c001404fe7f0b65203300b2bca0446cf0760030
Ghosty <75956731+Kurokeida@users.noreply.github.com>
2023-09-16 22:52:01 +0900

    Add files via upload

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.calculator
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "WrongViewCast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        var AnswerDisplay = findViewById<TextView>(R.id.AnswerView)
+        var FirstValue = findViewById<EditText>(R.id.FirstValue)
+        var SecondValue = findViewById<EditText>(R.id.SecondValue)
+
+        var PlusButton = findViewById<Button>(R.id.PlusButton)
+        var MinusButton = findViewById<Button>(R.id.MinusButton)
+        var TimesButton = findViewById<Button>(R.id.TimesButton)
+        var DivideButton = findViewById<Button>(R.id.DivideButton)
+        var SQRTButton = findViewById<Button>(R.id.SQRTButton)
+        var PowerButton = findViewById<Button>(R.id.PowerButton)
+
+
+        PlusButton.setOnClickListener {
+           var DifferenceStorage = FirstValue.text.toString().toInt() + SecondValue.text.toString().toInt()
+            AnswerDisplay.text = DifferenceStorage.toString()
+        }
+        MinusButton.setOnClickListener {
+            var DifferenceStorage = FirstValue.text.toString().toInt() - SecondValue.text.toString().toInt()
+            AnswerDisplay.text = DifferenceStorage.toString()
+        }
+        TimesButton.setOnClickListener {
+            var DifferenceStorage = FirstValue.text.toString().toInt() * SecondValue.text.toString().toInt()
+            AnswerDisplay.text = DifferenceStorage.toString()
+        }
+        DivideButton.setOnClickListener {
+            var DifferenceStorage = FirstValue.text.toString().toInt() / SecondValue.text.toString().toInt()
+            AnswerDisplay.text = DifferenceStorage.toString()
+        }
+        SQRTButton.setOnClickListener {
+            var DifferenceStorage = Math.sqrt(FirstValue.text.toString().toInt().toDouble())
+            AnswerDisplay.text = DifferenceStorage.toString()
+        }
+        PowerButton.setOnClickListener {
+
+            fun power(base: Int,power:Int): Int {
+                var result = 1
+                for (i in 1 until SecondValue.text.toString().toInt()) {
+                    result *= FirstValue.text.toString().toInt()
+                }
+                var DifferenceStorage = result
+                return result
+            }
+
+        }
+
+
+    }
+}
